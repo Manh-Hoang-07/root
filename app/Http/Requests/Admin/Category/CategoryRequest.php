d2efb669559b9d14e660f4c7ada56f3a47cfc859
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2025-07-21 02:30:00 +0700

    cập nhật lại validate cho api category

---
+++ app/Http/Requests/Admin/Category/CategoryRequest.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Http\Requests\Admin\Category;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:100',
+            'description' => 'nullable|string|max:255',
+            'parent_id' => 'nullable|exists:categories,id',
+            'status' => 'required|in:active,inactive',
+            'image' => 'nullable|string|max:255',
+        ];
+    }
+} 
--- app/Services/Category/CategoryService.php
+++ app/Services/Category/CategoryService.php
@@ -3,11 +3,28 @@ namespace App\Services\Category;
 
 use App\Repositories\Category\CategoryRepository;
 use App\Services\BaseService;
+use Illuminate\Support\Str;
 
 class CategoryService extends BaseService
 {
     public function __construct(CategoryRepository $repo)
     {
         parent::__construct($repo);
+    }
+
+    public function create($data)
+    {
+        if (!empty($data['name'])) {
+            $data['slug'] = Str::slug($data['name']);
+        }
+        return parent::create($data);
+    }
+
+    public function update($id, $data)
+    {
+        if (!empty($data['name'])) {
+            $data['slug'] = Str::slug($data['name']);
+        }
+        return parent::update($id, $data);
     }
 } 
