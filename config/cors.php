3cb94f2e5c19d8719351fe96edde3db8c8421db7
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2025-08-08 20:24:19 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:8000', 'http://127.0.0.1:8000'],
+    'allowed_origins' => env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:8000,http://127.0.0.1:8000') ? explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,http://localhost:8000,http://127.0.0.1:8000')) : [],
 
     'allowed_origins_patterns' => [],
 
